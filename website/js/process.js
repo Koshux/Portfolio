11b8aa3fe50894d76e6b399407cc6568864e301a
lanzonprojects <lanzonprojects@gmail.com>
2017-05-27 01:23:16 +0200

    Updated JS
    Fixed syntax bugs.

--- website/js/process.js
+++ website/js/process.js
@@ -2,7 +2,12 @@ function Process () {
 
 	this.viewModel = {
 	    firstName: ko.observable('James'),
-	    lastName: ko.observable('Lanzon')
+	    lastName: ko.observable('Lanzon'),
+        design: {
+            raysJewellery: function (element) {
+                console.log(element)
+            }
+        }
 	}
 
 	this.fullName = ko.pureComputed(function () {
@@ -10,14 +15,14 @@ function Process () {
 	}, this)
 
 	this.init()
-	showName()
+	this.showName()
 }
 
 Process.prototype.init = function () {
-	ko.applyBindings(viewModel)
+	ko.applyBindings(this.viewModel)
 }
 
 Process.prototype.showName = function () {
-	console.log(fullName())
+	console.log(this.fullName())
 }
 
